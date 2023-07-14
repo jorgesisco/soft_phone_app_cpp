f952ba55b3d4c923ce47caee09e4847ef58ba1d4
Jorge Sisco <jorgesisco17@gmail.com>
2023-07-14 06:05:13 +0200

    removed onCallMediaState for now

--- src/MyCall.cpp
+++ src/MyCall.cpp
@@ -29,27 +29,27 @@ void MyCall::onCallState(pj::OnCallStateParam &prm)
         }
     }
 
-void MyCall::onCallMediaState(pj::OnCallMediaStateParam &prm)
-{
-        pj::CallInfo ci = getInfo();
+// void MyCall::onCallMediaState(pj::OnCallMediaStateParam &prm)
+// {
+//         pj::CallInfo ci = getInfo();
 
-        for (auto &mi : ci.media)
-        {
-            if (mi.type == PJMEDIA_TYPE_AUDIO && mi.status == PJSUA_CALL_MEDIA_ACTIVE)
-            {
-                pj::AudioMedia audioMedia = getAudioMedia(mi.index);
+//         for (auto &mi : ci.media)
+//         {
+//             if (mi.type == PJMEDIA_TYPE_AUDIO && mi.status == PJSUA_CALL_MEDIA_ACTIVE)
+//             {
+//                 pj::AudioMedia audioMedia = getAudioMedia(mi.index);
                 
 
-                // Create a reference to playback device media
-                const pj::AudioMedia& playbackMedia = pj::Endpoint::instance().audDevManager().getPlaybackDevMedia();
+//                 // Create a reference to playback device media
+//                 const pj::AudioMedia& playbackMedia = pj::Endpoint::instance().audDevManager().getPlaybackDevMedia();
 
-                // Transmit the call's audio media to the capture device (for you to hear the caller)
-                audioMedia.startTransmit(playbackMedia);
+//                 // Transmit the call's audio media to the capture device (for you to hear the caller)
+//                 audioMedia.startTransmit(playbackMedia);
 
-                // Transmit from the local machine's microphone to the call's audio media
-                pj::Endpoint::instance().audDevManager().getCaptureDevMedia().startTransmit(audioMedia);
-            }
-        }
-    }
+//                 // Transmit from the local machine's microphone to the call's audio media
+//                 pj::Endpoint::instance().audDevManager().getCaptureDevMedia().startTransmit(audioMedia);
+//             }
+//         }
+//     }
 
 
